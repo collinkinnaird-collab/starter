ec58afec5f4b656580c4422796ae32fb0fc8152c
Collin Kinnaird <collinkinnaird@gmail.com>
2026-02-13 16:35:07 -0700

    progress

--- src/app.jsx
+++ src/app.jsx
@@ -3,5 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
-  return <div className="body bg-dark text-light">App will display here</div>;
+  return <div className="app bg-dark text-light">
+    
+  </div>;
 }
