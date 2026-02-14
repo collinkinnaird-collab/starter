c079254938dd6e7cd3c5bec4ec37c721afceae41
Collin Kinnaird <collinkinnaird@gmail.com>
2026-02-13 19:19:00 -0700

    fixed some image brokenness

--- src/friends/friends.jsx
+++ src/friends/friends.jsx
@@ -8,21 +8,21 @@ export function Friends() {
             <div className="scroll-panel"/>
                 <div className="row g-3 mb-3">
                     <div className="col-6 col-lg-3"><div className="card p-3 custom">
-                        <img src="../images/ChatGPT Image Jan 27, 2026 at 07_41_48 PM.png" alt="settings" width="50"/><NavLink to="/friends/:id">View Profile</NavLink>
+                        <img src="../images/image_2.png" alt="settings" width="50"/><NavLink to="/friends/:id">View Profile</NavLink>
                         <p className="small">GoalMaster27</p>
                     </div></div>
                 </div>
                 
                 <div className="row g-3 mb-3">
                     <div className="col-6 col-lg-3"><div className="card p-3 custom">
-                        <img src="../images/ChatGPT Image Jan 27, 2026 at 07_41_48 PM.png" alt="settings" width="50"/><NavLink to="/friends/:id">View Profile</NavLink>
+                        <img src="../images/image_2.png" alt="settings" width="50"/><NavLink to="/friends/:id">View Profile</NavLink>
                         <p className="small">epicGirl</p>
                     </div></div>
                 </div>
 
                 <div className="row g-3 mb-3">
                     <div className="col-6 col-lg-3"><div className="card p-3 custom">
-                        <img src="../images/ChatGPT Image Jan 27, 2026 at 07_41_48 PM.png" alt="settings" width="50"/><NavLink to="/friends/:id">View Profile</NavLink>
+                        <img src="../images/image_2.png" alt="settings" width="50"/><NavLink to="/friends/:id">View Profile</NavLink>
                         <p className="small">User Name</p>
                     </div></div>
                 </div>
