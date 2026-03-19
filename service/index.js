2b8931c2b349ae5a11ba89ddc1120f115bb8b508
Collin Kinnaird <collinkinnaird@gmail.com>
2026-03-19 09:56:04 -0600

    transitioning from local storage to DB

--- service/index.js
+++ service/index.js
@@ -14,8 +14,6 @@ const authCookieName = 'token';
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
 app.use(express.static('public'));
 
-let users = [];
-
 app.use(express.json());
 app.use(cookieParser());
 
@@ -41,6 +39,7 @@ apiRouter.post('/auth/login', async (req, res) => {
   if (user) {
     if (await bcrypt.compare(req.body.password, user.password)) {
       user.token = uuid.v4();
+      await DB.updateUser(user);
       setAuthCookie(res, user.token);
       res.send({ email: user.email });
       return;
@@ -53,7 +52,8 @@ apiRouter.post('/auth/login', async (req, res) => {
 apiRouter.delete('/auth/logout', async (req, res) => {
   const user = await findUser('token', req.cookies[authCookieName]);
   if (user) {
-    delete user.token;
+    await DB.updateUserRemoveAuth(user);
+    //delete user.token;
   }
   res.clearCookie(authCookieName);
   res.status(204).end();
@@ -109,7 +109,8 @@ async function createUser(email, password) {
     password: passwordHash,
     token: uuid.v4(),
   };
-  users.push(user);
+  await DB.addUser(user);
+  //users.push(user);
 
   return user;
 }
@@ -117,7 +118,10 @@ async function createUser(email, password) {
 async function findUser(field, value) {
   if (!value) return null;
 
-  return users.find((u) => u[field] === value);
+  if (field === 'token') {
+    return DB.getUserByToken(value);
+  }
+  return DB.getUser(value);
 }
 
 // setAuthCookie in the HTTP response
